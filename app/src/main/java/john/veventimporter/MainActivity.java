478d44cf67a7033763f81e663315220e3139c0f0
johvik <john.viklund@gmail.com>
2014-06-21 20:54:33 +0200

    Removed settings

--- app/src/main/java/john/veventimporter/MainActivity.java
+++ app/src/main/java/john/veventimporter/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 import john.veventimporter.fragments.MainFragment;
@@ -35,22 +33,6 @@ public class MainActivity extends Activity implements UriSelectFragment
             mMainFragment = MainFragment.newInstance(uri);
             getFragmentManager().beginTransaction().add(R.id.container, mMainFragment).commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
     @Override
